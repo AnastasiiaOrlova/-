4e348e2e1b6f60d4d4e13efc0e005bd02d3342c0
Anastasiia Orlova <an.u.orlova@mail.ru>
2022-08-08 00:59:18 +0300

    Add task number 3 from seminar1

---
+++ Seminars/Seminar1/Task_number3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Input integer possitive number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int current = number * (-1);
+while (current <= number)
+{
+    Console.Write(current + " ");
+    current ++;
+}
