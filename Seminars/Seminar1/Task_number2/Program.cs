ee6839ef511164a797488478b4ebc9c9031120c7
Anastasiia Orlova <an.u.orlova@mail.ru>
2022-08-08 00:39:47 +0300

    Add task number 2 from seminar1

---
+++ Seminars/Seminar1/Task_number2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write ("Input first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int square2 = n2 * n2;
+if (n1 == square2)
+    Console.WriteLine ($"{n1} is square of {n2}");
+else 
+    Console.WriteLine ($"{n1} is not square of {n2}");
