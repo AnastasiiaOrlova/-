c44433eaed68b8e11608589531e1417d5b944d60
Anastasiia Orlova <an.u.orlova@mail.ru>
2022-08-08 01:22:57 +0300

    Add task number 4 from seminar1

---
+++ Seminars/Seminar1/Task_number4/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Input integer three-digit number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int ed = number % 10;
+Console.WriteLine($"Last digit of {number} is {ed}");
