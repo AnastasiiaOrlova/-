783b4b6f02fff433f17730525355664d7f64d042
Anastasiia Orlova <an.u.orlova@mail.ru>
2022-08-09 00:55:27 +0300

    Add massives from lesson2

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -78,4 +78,113 @@ int max = Max(
 Console.WriteLine(max);
 */
 
+
+/*
 //Example004
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int [] array = {11, 21, 31, 41, 15, 61,17,18,19};
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
+*/
+
+
+/*
+//Example005
+int[] array = {1, 12, 31, 4, 15, 18, 16, 17, 18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+        index ++;
+}
+*/
+
+
+/*
+//Example006
+void FillArray(int[]collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index = index +1;
+    }
+}
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[]array = new int [10];
+FillArray(array);
+PrintArray(array);
+
+*/
+
+//Example007
+void FillArray(int[]collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index = index +1;
+    }
+}
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = -1;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index]==find)
+        {
+            position = index;
+            break;
+        }
+        index ++;
+    }
+    return position;
+}
+int[]array = new int [10];
+FillArray(array);
+array[4] = 4;
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
