d93e6d974a65543a9d691a84c2aa44b0cb53d65e
Anastasiia Orlova <an.u.orlova@mail.ru>
2022-08-08 21:33:56 +0300

    Add task number 1 from home work number 1

---
+++ Home_Works/Home_work1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+*/
+Console.Write("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Max = {number1}, Min = {number2}");
+}
+else
+{
+    Console.WriteLine($"Max = {number2}, Min = {number1}");
+}
