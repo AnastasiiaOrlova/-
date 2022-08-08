e9d0881917557040f313daccff73deab718fdf46
Anastasiia Orlova <an.u.orlova@mail.ru>
2022-08-08 22:39:08 +0300

    Add task number 4 from home work number 1

--- Home_Works/Home_work1/Program.cs
+++ Home_Works/Home_work1/Program.cs
@@ -16,6 +16,7 @@ else
 */
 
 
+
 /*
 // 2. Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
@@ -37,8 +38,10 @@ if (number3 > max)
 Console.WriteLine($"Maximal number is {max}.");
 */
 
+
+
 /*
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// 3. Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
 // является ли число чётным (делится ли оно на два без остатка).
 
 Console.Write("Input integer number: ");
@@ -53,3 +56,28 @@ else
 }
 */
 
+
+/*
+// 4. Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Input integer positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+if (number % 2 == 0)
+{
+    while (current <= number)
+    {
+        Console.Write(current + " ");
+        current = current + 2;
+    }
+}    
+else
+{
+    while (current < number)
+    {
+        Console.Write(current + " ");
+        current = current + 2;
+    }
+    
+}    
+
+*/
